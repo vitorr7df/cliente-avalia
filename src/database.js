ef2857252c83600d8d79a02b3131706b6ec971fe
vitorr7df <vitor.rochadf@gmail.com>
2023-07-19 16:09:43 -0300

    adc prisma3

--- src/database.js
+++ src/database.js
@@ -1,33 +1,6 @@
-const { Client } = require('pg');
-require('dotenv').config();
+// database.js
+const { PrismaClient } = require('@prisma/client');
 
-const URL = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}/${process.env.PGDATABASE}?sslmode=require&options=project%3D${process.env.ENDPOINT_ID}`;
+const prismaClient = new PrismaClient();
 
-const connect = async (formData) => {
-    try {
-        const client = new Client(URL);
-        await client.connect();
-
-        // console.log(formData)
-        await client.query(
-            `INSERT INTO satisfacao (pergunta_um, pergunta_dois, pergunta_tres, pergunta_quatro, pergunta_cinco, obs, nome)
-             VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-            [
-                formData.p1,
-                formData.p2,
-                formData.p3,
-                formData.p4,
-                formData.p5,
-                formData.obs,
-                formData.nome
-            ]
-        );
-
-        await client.end();
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-
-module.exports = { connect };
+module.exports = { prismaClient };
